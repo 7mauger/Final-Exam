54d340f8247c05733328e1f77783229c90cc89f3
7mauger <7mauger@stpatrick.org>
2016-12-14 11:23:57 -0600

    Add files via upload

--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -6,9 +6,27 @@ class Password{
   }
   //Instance functions below this comment.
 validPublicKey(){
-  while(this.publickey.length)
+  while(this.publickey.length > 8 && this.publickey.length < 25){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
-
-
   //Static function below this comment.
+  static makePrivateKey(){
+    let key = "";
+    let limit = 14;
+    while(var x=0;x<limit;x++){
+      if(x==4 || x==9){
+        key += "-"
+      }
+      else{
+        var number = Math.random()*10;
+        number = Math.floor(number);
+        key += number
+      }
+    }
+    return key;
+  }
 }
